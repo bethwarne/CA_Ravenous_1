84052a75e4cf6ad5a66996844e326a0b3b6a1698
bethwarne <39646549+bethwarne@users.noreply.github.com>
2018-07-13 12:47:54 +0100

    Add files via upload

---
+++ src/components/BusinessList/businessList.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './businessList.css';
+
+import Business from '../Business/business';
+
+class BusinessList extends React.Component {
+  render() {
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} />
+          })
+        }
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
